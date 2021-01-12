47dd9f2cedc32b5f3582c8f0997811664dc24cb5
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-12 19:42:10 -0300

    add Authentication Use Case

---
+++ lib/domain/usecases/authentication.dart
@@ -0,0 +1,11 @@
+
+import 'package:meta/meta.dart';
+
+import '../entities/entities.dart';
+
+abstract class Authentication {
+  Future<AccountEntity> auth(
+    @required String email,
+    @required String password
+  );
+}
