8cf6251d00de03bd8a294732fe47455bae4f5307
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-12 22:18:14 -0300

    change Authentication usecase to receive a model

--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -4,8 +4,14 @@ import 'package:meta/meta.dart';
 import '../entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth(
-    @required String email,
-    @required String password
-  );
+  Future<AccountEntity> auth(@required AuthenticationParams);
+}
+
+class AuthenticationParams {
+  final String email;
+  final String secret;
+  AuthenticationParams({
+    @required this.email,
+    @required this.secret
+  });
 }
