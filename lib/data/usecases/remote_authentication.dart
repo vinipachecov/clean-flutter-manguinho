01ef57693f2f2ecee1cb5274b9e27d554f70249b
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-15 09:48:38 -0300

    make RemoteAuthentication implements Authentication

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -8,7 +8,7 @@ import '../../domain/entities/entities.dart';
 import '../models/models.dart';
 import '../http/http_client.dart';
 
-class RemoteAuthentication {
+class RemoteAuthentication implements Authentication {
   final HttpClient httpClient;
   final String url;
 
--- lib/domain/usecases/authentication.dart
+++ lib/domain/usecases/authentication.dart
@@ -4,7 +4,7 @@ import 'package:meta/meta.dart';
 import '../entities/entities.dart';
 
 abstract class Authentication {
-  Future<AccountEntity> auth(@required AuthenticationParams);
+  Future<AccountEntity> auth(AuthenticationParams params);
 }
 
 class AuthenticationParams {
