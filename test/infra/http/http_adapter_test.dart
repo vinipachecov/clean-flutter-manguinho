2c3f57a122203e18916286a9cf559af535578599
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-15 21:28:08 -0300

    ensure HtppAdapter returns null if post returns 200 with no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -22,7 +22,7 @@ class HttpAdapter {
     };
     final jsonBody = body != null ? jsonEncode(body) : null;
     final response = await client.post(url, headers: headers, body: jsonBody);
-    return jsonDecode(response.body);
+    return response.body.isEmpty ? null : jsonDecode(response.body);
   }
 }
 
@@ -72,11 +72,14 @@ void main() {
       final response = await sut.request(url: url, method: 'post');
 
       expect(response, { 'any_key': 'any_value'});
+    });
 
-      verify(client.post(
-        url,
-        headers: anyNamed('headers'),
-      ));
+    test('Should return null if post returns 200 with no data', () async {
+      when(client.post(any, headers: anyNamed('headers')))
+      .thenAnswer((_) async => Response('', 200));
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
     });
   });
 }
