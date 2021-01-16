a1a3293ff088a4de67d29bcbeeaf117e6ae1e7d6
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-15 22:51:30 -0300

    ensure HttpAdapter throws UnauthorizedError  on 401

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -98,5 +98,12 @@ void main() {
 
       expect(future, throwsA(HttpError.serverError));
     });
+
+    test('Should return UnauthorizedError if post returns 401', () async {
+      mockResponse(401);
+      final future = sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.unauthorized));
+    });
   });
 }
