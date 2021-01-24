0308fef1d9799f71febd87c5fd2a38735fe01db0
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-24 15:47:29 -0300

    add type to streams

--- lib/ui/pages/login/login_presenter.dart
+++ lib/ui/pages/login/login_presenter.dart
@@ -1,9 +1,9 @@
 abstract class LoginPresenter {
   Stream get emailErrorStream;
-  Stream get passwordErrorStream;
-  Stream get isFormValidStream;
-  Stream get isLoadingStream;
-  Stream get mainErrorStream;
+  Stream<String> get passwordErrorStream;
+  Stream<String> get isFormValidStream;
+  Stream<bool> get isLoadingStream;
+  Stream<String> get mainErrorStream;
 
   void validateEmail(String email);
   void validatePassword(String password);
