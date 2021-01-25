5f4d98717161eeb0cf391ab87d3e95e8ed71e319
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-25 19:36:58 -0300

    move code to its own file

--- test/validation/validators/email_validation_test.dart
+++ test/validation/validators/email_validation_test.dart
@@ -1,18 +1,6 @@
-import 'package:clean_flutter_manguinho/validation/protocols/field_validation.dart';
 import 'package:test/test.dart';
 
-class EmailValidation implements FieldValidation {
-  final String field;
-
-  EmailValidation(this.field);
-
-  @override
-  String validate(String value) {
-    final regex = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
-    final isValid = value?.isNotEmpty != true || regex.hasMatch(value);
-    return isValid ? null : 'Campo inválido';
-  }
-}
+import 'package:clean_flutter_manguinho/validation/validators/validators.dart';
 
 void main() {
   EmailValidation sut;
