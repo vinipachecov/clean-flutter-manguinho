2604d9732a6c2742f91920a4d85ba0d7f3d07398
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-25 22:09:32 -0300

    move ValidationComposite test setup to a helper method

--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -17,17 +17,43 @@ class ValidationComposite implements Validation {
 
 class FieldValidationSpy extends Mock implements FieldValidation {}
 
-main() {
+  main() {
+    FieldValidationSpy validation1;
+    FieldValidationSpy validation2;
+    FieldValidationSpy validation3;
+    ValidationComposite sut;
+
+  void mockValidation1(String error) {
+    when(validation1.field).thenReturn(error);
+  }
+
+  void mockValidation2(String error) {
+    when(validation2.field).thenReturn(error);
+  }
+
+  void mockValidation3(String error) {
+    when(validation3.field).thenReturn(error);
+  }
+
+  setUp(() {
+    validation1 = FieldValidationSpy();
+    when(validation1.field).thenReturn('any_field');
+    mockValidation1(null);
+
+
+    validation2 = FieldValidationSpy();
+    when(validation2.field).thenReturn('any_field');
+    mockValidation2(null);
+
+    validation3 = FieldValidationSpy();
+    when(validation3.field).thenReturn('other_field');
+    mockValidation3(null);
+
+    sut = ValidationComposite([validation1, validation2]);
+  });
 
   test('Should return null if all validations returns null or empty', () {
-    final validation1 = FieldValidationSpy();
-    final validation2 = FieldValidationSpy();
-    when(validation1.field).thenReturn('any_field');
-    when(validation1.validate(any)).thenReturn(null);
-    when(validation2.field).thenReturn('any_field');
-    when(validation2.validate(any)).thenReturn('');
-    final sut = ValidationComposite([validation1, validation2]);
-
+    mockValidation2('');
     final error = sut.validate(field: 'any_field', value: 'any_value');
 
     expect(error, null);
