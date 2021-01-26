47654750d5cb6b7c5b417b67e2161114bf7a5263
Vinicius Vieira <vinipachecov@gmail.com>
2021-01-25 22:41:38 -0300

    move ValidationComposite to its own file

---
+++ lib/validation/validators/validation_composite.dart
@@ -0,0 +1,22 @@
+
+import '../../validation/protocols/protocols.dart';
+import '../../presentation/protocols/protocols.dart';
+import 'package:meta/meta.dart';
+
+class ValidationComposite implements Validation {
+  final List<FieldValidation> validations;
+
+  ValidationComposite(this.validations);
+
+  @override
+  String validate({@required String field, @required String value}) {
+    String error;
+    for (final validation in validations.where((v) => v.field == field)) {
+        final error = validation.validate(value);
+        if (error?.isNotEmpty == true) {
+          return error;
+        }
+    }
+    return error;
+  }
+}
--- test/validation/validators/validation_composite_test.dart
+++ test/validation/validators/validation_composite_test.dart
@@ -1,26 +1,9 @@
-import 'package:clean_flutter_manguinho/presentation/protocols/validation.dart';
-import 'package:clean_flutter_manguinho/validation/protocols/field_validation.dart';
+
 import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
-import 'package:meta/meta.dart';
 
-class ValidationComposite implements Validation {
-  final List<FieldValidation> validations;
-
-  ValidationComposite(this.validations);
-
-  @override
-  String validate({@required String field, @required String value}) {
-    String error;
-    for (final validation in validations.where((v) => v.field == field)) {
-        final error = validation.validate(value);
-        if (error?.isNotEmpty == true) {
-          return error;
-        }
-    }
-    return error;
-  }
-}
+import 'package:clean_flutter_manguinho/validation/validators/validators.dart';
+import 'package:clean_flutter_manguinho/validation/protocols/protocols.dart';
 
 class FieldValidationSpy extends Mock implements FieldValidation {}
 
