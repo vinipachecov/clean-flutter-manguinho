1f8eb666c52d3021ca9f2137ccafe5e8591716e8
Vinicius Vieira <vinipachecov@gmail.com>
2021-03-18 20:51:48 -0300

    move splash page to its own file

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,29 @@
+import 'package:get/get.dart';
+
+import './splash_presenter.dart';
+import 'package:flutter/material.dart';
+
+
+class SplashPage extends StatelessWidget {
+  final SplashPresenter presenter;
+
+  SplashPage({@required this.presenter});
+
+  @override
+  Widget build(BuildContext context) {
+    presenter.loadCurrentAccount();
+
+    return Scaffold(
+      appBar: AppBar(title: Text('Company'),),
+      body: Builder(builder: (context) {
+        presenter.navigateToStream.listen((page) {
+          if (page?.isNotEmpty == true) {
+            Get.offAllNamed(page);
+          }
+        });
+
+        return Center(child: CircularProgressIndicator());
+      },)
+    );
+  }
+}
--- test/ui/pages/splash_page_test.dart
+++ test/ui/pages/splash_page_test.dart
@@ -1,38 +1,11 @@
 import 'dart:async';
 
+import 'package:clean_flutter_manguinho/ui/pages/splash/splash.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/route_manager.dart';
 import 'package:mockito/mockito.dart';
 
-class SplashPage extends StatelessWidget {
-  final SplashPresenter presenter;
-
-  SplashPage({@required this.presenter});
-
-  @override
-  Widget build(BuildContext context) {
-    presenter.loadCurrentAccount();
-
-    return Scaffold(
-      appBar: AppBar(title: Text('Company'),),
-      body: Builder(builder: (context) {
-        presenter.navigateToStream.listen((page) {
-          if (page?.isNotEmpty == true) {
-            Get.offAllNamed(page);
-          }
-        });
-
-        return Center(child: CircularProgressIndicator());
-      },)
-    );
-  }
-}
-
-abstract class SplashPresenter {
-  Stream<String> get navigateToStream;
-  Future<void> loadCurrentAccount();
-}
 class SplashPresenterSpy extends Mock implements SplashPresenter {}
 
 void main () {
