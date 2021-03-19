4c4571502720d3d4078df38bb62896df459059cb
Vinicius Vieira <vinipachecov@gmail.com>
2021-03-18 21:59:46 -0300

    ensure GetxSplashPresenter emits login  page on empty result

--- test/presentation/presenters/getx_splash_presenter_test.dart
+++ test/presentation/presenters/getx_splash_presenter_test.dart
@@ -1,5 +1,7 @@
+import 'package:clean_flutter_manguinho/domain/entities/account_entity.dart';
 import 'package:clean_flutter_manguinho/domain/usecases/load_current_account.dart';
 import 'package:clean_flutter_manguinho/ui/pages/splash/splash_presenter.dart';
+import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:get/state_manager.dart';
 import 'package:mockito/mockito.dart';
@@ -16,8 +18,8 @@ class GetxSplashPresenter implements SplashPresenter {
   GetxSplashPresenter({@required this.loadCurrentAccount});
 
   Future<void> checkAccount() async {
-    await loadCurrentAccount.load();
-    _navigateTo.value = '/surveys';
+    final account = await loadCurrentAccount.load();
+    _navigateTo.value = account == null ? '/login' : '/surveys' ;
   }
 }
 
@@ -27,9 +29,14 @@ void main() {
   LoadCurrentAccountSpy loadCurrentAccount;
   GetxSplashPresenter sut;
 
+  void mockLoadCurrentAccount({AccountEntity account}) {
+    when(loadCurrentAccount.load()).thenAnswer((_) async => account);
+  }
+
   setUp(() {
     loadCurrentAccount = LoadCurrentAccountSpy();
      sut = GetxSplashPresenter(loadCurrentAccount: loadCurrentAccount);
+     mockLoadCurrentAccount(account: AccountEntity(faker.guid.guid()));
   });
 
   test('Should call LoadCurrentAccount', () async {
@@ -42,5 +49,13 @@ void main() {
     sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/surveys')));
 
       await sut.checkAccount();
+  });
+
+  test('Should go to login page on null result', () async {
+    mockLoadCurrentAccount(account: null);
+
+    sut.navigateToStream.listen(expectAsync1((page) => expect(page, '/login')));
+
+    await sut.checkAccount();
   });
 }
