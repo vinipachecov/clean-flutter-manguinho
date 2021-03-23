0d8ac5887c1466a3093a8a29f43f06427f790325
Vinicius Vieira <vinipachecov@gmail.com>
2021-03-23 09:20:01 -0300

    add addAccount use case

---
+++ lib/domain/usecases/add_account.dart
@@ -0,0 +1,25 @@
+
+import 'package:meta/meta.dart';
+
+import '../entities/entities.dart';
+import 'package:equatable/equatable.dart';
+
+abstract class AddAccount {
+  Future<AccountEntity> add(AddAccountParams params);
+}
+
+class AddAccountParams extends Equatable {
+  final String name;
+  final String email;
+  final String password;
+  final String passwordConfirmation;
+
+
+  List get props => [email,name, password, passwordConfirmation];
+  AddAccountParams({
+    @required this.name,
+    @required this.email,
+    @required this.password,
+    @required this.passwordConfirmation,
+  });
+}
