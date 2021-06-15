d0730d7793593023018dc60f9facec8e37021a4e
Vinicius Vieira <vinipachecov@gmail.com>
2021-06-15 09:15:00 -0300

    ensure CompareFieldsValidation returns null if values are equal

--- test/validation/validators/compare_fields_validation_test.dart
+++ test/validation/validators/compare_fields_validation_test.dart
@@ -1,4 +1,3 @@
-import 'package:faker/faker.dart';
 import 'package:test/test.dart';
 
 import 'package:clean_flutter_manguinho/presentation/protocols/validation.dart';
@@ -10,7 +9,11 @@ void main() {
   setUp(() {
     sut = CompareFieldsValidation(field: 'any_field', valueToCompare: 'any_value');
   });
-  test('Should return error if value is empty', () {
+  test('Should return error if values are not equal', () {
     expect(sut.validate('wrong_value'), ValidationError.invalidField);
+  });
+
+  test('Should return null if values are equal', () {
+    expect(sut.validate('any_value'), null);
   });
 }
