564e9b8aaed5d99f55683b71eff1570deca3fca6
Vinicius Vieira <vinipachecov@gmail.com>
2021-06-23 09:42:55 -0300

    ensure HttpAdapter calls get throws ServerError if HttpClient throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -148,6 +148,11 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
+
     setUp(() {
         mockResponse(200);
     });
@@ -226,6 +231,13 @@ void main() {
 
     test('Should return ServerError if get returns 500', () async {
       mockResponse(500);
+      final future = sut.request(url: url, method: 'get');
+
+      expect(future, throwsA(HttpError.serverError));
+    });
+
+    test('Should return ServerError if get throws', () async {
+      mockError();
       final future = sut.request(url: url, method: 'get');
 
       expect(future, throwsA(HttpError.serverError));
