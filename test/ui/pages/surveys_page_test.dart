bbc26245b66f042ef44965488d1aa34574d4b666
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2021-07-12 19:21:59 -0300

    move Load Surveys page setup to a helper method

--- test/ui/pages/surveys_page_test.dart
+++ test/ui/pages/surveys_page_test.dart
@@ -4,21 +4,22 @@ import 'package:clean_flutter_manguinho/ui/pages/pages.dart';
 import 'package:mockito/mockito.dart';
 
 class SurveysPresenterSpy extends Mock implements SurveysPresenter {}
-void main() {
 
-  testWidgets('Should call LoadSurveys on page load', (WidgetTester tester) async {
-    final presenter = SurveysPresenterSpy();
-    final surveysPage = GetMaterialApp(
-      initialRoute: '/surveys',
-      getPages: [
-        GetPage(name: '/surveys', page: () => SurveysPage(presenter))
-      ]
-    );
+void main() {
+  SurveysPresenterSpy presenter;
+
+  Future<void> loadPage(WidgetTester tester) async {
+    presenter = SurveysPresenterSpy();
+    final surveysPage = GetMaterialApp(initialRoute: '/surveys', getPages: [
+      GetPage(name: '/surveys', page: () => SurveysPage(presenter))
+    ]);
 
     await tester.pumpWidget(surveysPage);
+  }
 
+  testWidgets('Should call LoadSurveys on page load',
+      (WidgetTester tester) async {
+    loadPage(tester);
     verify(presenter.loadData()).called(1);
-
   });
-
 }
