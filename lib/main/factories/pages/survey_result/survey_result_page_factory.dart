af811ed1bfb39c9231a77fed07253c9e39c85fc4
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2021-08-04 20:56:43 -0300

    ensure SurveyResultPage calls LoadSurveyResult on page load

--- lib/main/factories/pages/survey_result/survey_result_page_factory.dart
+++ lib/main/factories/pages/survey_result/survey_result_page_factory.dart
@@ -2,5 +2,5 @@ import '../../../../ui/pages/pages.dart';
 import 'package:flutter/material.dart';
 
 Widget makeSurveyResultPage() {
-  return SurveyResultPage();
+  return SurveyResultPage(null);
 }
