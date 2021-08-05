2d86ee7f39538b88f4025faaab0d246d1cb553a3
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2021-08-05 19:20:24 -0300

    move components to their own files

---
+++ lib/ui/pages/survey_result/components/survey_answer.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import './components.dart';
+import 'package:clean_flutter_manguinho/ui/pages/survey_result/survey_result.dart';
+
+class SurveyAnswer extends StatelessWidget {
+  const SurveyAnswer({
+    Key key,
+    @required this.viewModel,
+  }) : super(key: key);
+
+  final SurveyAnswerViewModel viewModel;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> _buildItems() {
+      List<Widget> children = [
+        Expanded(
+            child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Text(viewModel.answer, style: TextStyle(fontSize: 16)),
+        )),
+        Text(viewModel.percent,
+            style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Theme.of(context).primaryColorDark)),
+        viewModel.isCurrentAnswer ? ActiveIcon() : DisabledIcon()
+      ];
+      if (viewModel.image != null) {
+        children.insert(
+            0,
+            Image.network(
+              viewModel.image,
+              width: 40,
+            ));
+      }
+
+      return children;
+    }
+
+    return Column(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: Theme.of(context).backgroundColor,
+          ),
+          padding: const EdgeInsets.all(15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: _buildItems(),
+          ),
+        ),
+        Divider(height: 1)
+      ],
+    );
+  }
+}
--- lib/ui/pages/survey_result/components/survey_result.dart
+++ lib/ui/pages/survey_result/components/survey_result.dart
@@ -1,81 +1,21 @@
 import '../survey_result.dart';
 import 'package:flutter/material.dart';
+import './components.dart';
 
 class SurveyResult extends StatelessWidget {
-  final SurveyResultViewModel viewmodel;
-  SurveyResult(this.viewmodel);
+  final SurveyResultViewModel viewModel;
+  SurveyResult(this.viewModel);
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-        itemCount: viewmodel.answers.length + 1,
+        itemCount: viewModel.answers.length + 1,
         itemBuilder: (ctx, index) {
           if (index == 0) {
-            return Container(
-                padding:
-                    EdgeInsets.only(top: 40, bottom: 20, left: 20, right: 20),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).disabledColor.withAlpha(90),
-                ),
-                child: Text(viewmodel.question));
+            return SurveyHeader(viewModel.question);
           }
 
-          return Column(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).backgroundColor,
-                ),
-                padding: const EdgeInsets.all(15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    viewmodel.answers[index - 1].image != null
-                        ? Image.network(
-                            viewmodel.answers[index - 1].image,
-                            width: 40,
-                          )
-                        : SizedBox(height: 0),
-                    Expanded(
-                        child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 10),
-                      child: Text(viewmodel.answers[index - 1].answer,
-                          style: TextStyle(fontSize: 16)),
-                    )),
-                    Text(viewmodel.answers[index - 1].percent,
-                        style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).primaryColorDark)),
-                    viewmodel.answers[index - 1].isCurrentAnswer
-                        ? ActiveIcon()
-                        : DisabledIcon()
-                  ],
-                ),
-              ),
-              Divider(height: 1)
-            ],
-          );
+          return SurveyAnswer(viewModel: viewModel.answers[index - 1]);
         });
-  }
-}
-
-class ActiveIcon extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 10),
-      child: Icon(Icons.check_circle, color: Theme.of(context).highlightColor),
-    );
-  }
-}
-
-class DisabledIcon extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 10),
-      child: Icon(Icons.check_circle, color: Theme.of(context).disabledColor),
-    );
   }
 }
