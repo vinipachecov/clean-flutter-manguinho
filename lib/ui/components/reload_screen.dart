daa973f7d0b126f899660e241aab872d617deca9
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2021-08-04 21:10:59 -0300

    move ReloadScreen component to its own file

---
+++ lib/ui/components/reload_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:clean_flutter_manguinho/ui/helpers/helpers.dart';
+
+class ReloadScreen extends StatelessWidget {
+  final String error;
+  final Future<void> Function() reload;
+  const ReloadScreen({
+    @required this.error,
+    @required this.reload,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(40),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            error,
+            style: TextStyle(fontSize: 16),
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 10),
+          RaisedButton(
+            onPressed: reload,
+            child: Text(R.strings.reload),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,4 +1,4 @@
-import 'package:clean_flutter_manguinho/ui/components/spinner_dialog.dart';
+import 'package:clean_flutter_manguinho/ui/components/components.dart';
 import 'package:clean_flutter_manguinho/ui/pages/pages.dart';
 import 'package:clean_flutter_manguinho/ui/pages/surveys/survey_viewmodel.dart';
 import 'package:flutter/material.dart';
@@ -31,24 +31,8 @@ class SurveysPage extends StatelessWidget {
                 stream: presenter.surveysStream,
                 builder: (context, snapshot) {
                   if (snapshot.hasError) {
-                    return Padding(
-                      padding: const EdgeInsets.all(40),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            snapshot.error,
-                            style: TextStyle(fontSize: 16),
-                            textAlign: TextAlign.center,
-                          ),
-                          SizedBox(height: 10),
-                          RaisedButton(
-                            onPressed: presenter.loadData,
-                            child: Text(R.strings.reload),
-                          )
-                        ],
-                      ),
-                    );
+                    return ReloadScreen(
+                        error: snapshot.error, reload: presenter.loadData);
                   }
                   if (snapshot.hasData) {
                     return Padding(
