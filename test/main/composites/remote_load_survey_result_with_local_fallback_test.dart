a48c432825636671b7e24759b149f3f8a79683ca
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2021-08-17 09:42:42 -0300

    ensure Composite returns remote data

--- test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
+++ test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
@@ -1,21 +1,23 @@
-import 'package:clean_flutter_manguinho/domain/entities/entities.dart';
 import 'package:test/test.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:clean_flutter_manguinho/data/usecases/usecases.dart';
+import 'package:clean_flutter_manguinho/domain/entities/entities.dart';
+import 'package:clean_flutter_manguinho/domain/usecases/usecases.dart';
 
-class RemoteLoadSurveyResultWithLocalFallback {
+class RemoteLoadSurveyResultWithLocalFallback implements LoadSurveyResult {
   final RemoteLoadSurveyResult remote;
   final LocalLoadSurveyResult local;
 
   RemoteLoadSurveyResultWithLocalFallback(
       {@required this.remote, @required this.local});
 
-  Future<void> loadBySurvey({String surveyId}) async {
+  Future<SurveyResultEntity> loadBySurvey({String surveyId}) async {
     final surveyResult = await remote.loadBySurvey(surveyId: surveyId);
     await local.save(surveyId: surveyId, surveyResult: surveyResult);
+    return surveyResult;
   }
 }
 
@@ -62,5 +64,11 @@ void main() {
     await sut.loadBySurvey(surveyId: surveyId);
 
     verify(local.save(surveyId: surveyId, surveyResult: surveyResult));
+  });
+
+  test("Should return remote data", () async {
+    final response = await sut.loadBySurvey(surveyId: surveyId);
+
+    expect(response, surveyResult);
   });
 }
