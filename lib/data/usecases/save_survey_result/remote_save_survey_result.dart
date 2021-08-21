16c873c405ffd856e8c64b8cb0a2e91ec7779e29
Vinicius Pacheco Vieira <vinipachecov@gmail.com>
2021-08-21 20:25:34 -0300

    ensure RemoteSaveSurveyResult returns SurveyResult on success

--- lib/data/usecases/save_survey_result/remote_save_survey_result.dart
+++ lib/data/usecases/save_survey_result/remote_save_survey_result.dart
@@ -1,21 +1,21 @@
-import 'package:clean_flutter_manguinho/domain/entities/survey_result_entity.dart';
 import 'package:meta/meta.dart';
 import 'package:clean_flutter_manguinho/domain/helpers/helpers.dart';
 import 'package:clean_flutter_manguinho/domain/usecases/usecases.dart';
-
+import 'package:clean_flutter_manguinho/domain/entities/survey_result_entity.dart';
 import 'package:clean_flutter_manguinho/data/http/http.dart';
 import 'package:clean_flutter_manguinho/data/models/models.dart';
 
-class RemoteSaveSurveyResult {
+class RemoteSaveSurveyResult implements SaveSurveyResult {
   final String url;
   final HttpClient httpClient;
 
   RemoteSaveSurveyResult({@required this.url, @required this.httpClient});
 
-  Future<void> save(String answer) async {
+  Future<SurveyResultEntity> save({String answer}) async {
     try {
-      await httpClient
+      final json = await httpClient
           .request(url: url, method: 'put', body: {'answer': answer});
+      return RemoteSurveyResultModel.fromJson(json).toEntity();
     } on HttpError catch (error) {
       throw error == HttpError.forbidden
           ? DomainError.accessDenied
--- test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
+++ test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
@@ -17,6 +17,7 @@ void main() {
   HttpClientSpy httpClient;
   RemoteSaveSurveyResult sut;
   String answer;
+  Map surveyResult;
 
   PostExpectation mockRequest() => when(httpClient.request(
       url: anyNamed('url'),
@@ -25,23 +26,75 @@ void main() {
 
   void mockHttpError(error) => mockRequest().thenThrow(error);
 
+  Map mockValidData() => {
+        'surveyId': faker.guid.guid(),
+        'question': faker.randomGenerator.string(50),
+        'answers': [
+          {
+            'image': faker.internet.httpUrl(),
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          },
+          {
+            'answer': faker.randomGenerator.string(20),
+            'percent': faker.randomGenerator.integer(100),
+            'count': faker.randomGenerator.integer(1000),
+            'isCurrentAccountAnswer': faker.randomGenerator.boolean()
+          }
+        ],
+        'date': faker.date.dateTime().toIso8601String()
+      };
+
+  void mockHttpData(Map data) {
+    surveyResult = data;
+    mockRequest().thenAnswer((_) async => data);
+  }
+
   setUp(() {
     url = faker.internet.httpUrl();
     httpClient = HttpClientSpy();
     answer = faker.lorem.sentence();
     sut = RemoteSaveSurveyResult(url: url, httpClient: httpClient);
+    mockHttpData(mockValidData());
   });
   test('Should call HttpClient with correct values', () async {
-    await sut.save(answer);
+    await sut.save(answer: answer);
 
     verify(
         httpClient.request(url: url, method: 'put', body: {'answer': answer}));
   });
 
+  test('Should return survey result on 200', () async {
+    final result = await sut.save(answer: answer);
+
+    expect(
+        result,
+        SurveyResultEntity(
+            surveyId: surveyResult['surveyId'],
+            question: surveyResult['question'],
+            answers: [
+              SurveyAnswerEntity(
+                image: surveyResult['answers'][0]['image'],
+                answer: surveyResult['answers'][0]['answer'],
+                isCurrentAnswer: surveyResult['answers'][0]
+                    ['isCurrentAccountAnswer'],
+                percent: surveyResult['answers'][0]['percent'],
+              ),
+              SurveyAnswerEntity(
+                answer: surveyResult['answers'][1]['answer'],
+                isCurrentAnswer: surveyResult['answers'][1]
+                    ['isCurrentAccountAnswer'],
+                percent: surveyResult['answers'][1]['percent'],
+              )
+            ]));
+  });
+
   test('Should throw UnexpectedError if httpClient returns 404', () async {
     mockHttpError(HttpError.notFound);
 
-    final future = sut.save(answer);
+    final future = sut.save(answer: answer);
 
     expect(future, throwsA(DomainError.unexpected));
   });
@@ -49,7 +102,7 @@ void main() {
   test('Should throw UnexpectedError if httpClient returns 500', () async {
     mockHttpError(HttpError.serverError);
 
-    final future = sut.save(answer);
+    final future = sut.save(answer: answer);
 
     expect(future, throwsA(DomainError.unexpected));
   });
@@ -57,7 +110,7 @@ void main() {
   test('Should throw accessDenied if httpClient returns 403', () async {
     mockHttpError(HttpError.forbidden);
 
-    final future = sut.save(answer);
+    final future = sut.save(answer: answer);
 
     expect(future, throwsA(DomainError.accessDenied));
   });
